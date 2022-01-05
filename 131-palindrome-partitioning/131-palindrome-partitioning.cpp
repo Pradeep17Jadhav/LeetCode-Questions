7ac2e2f0ec95b925c05d00b27221ee846623f16a
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-05 19:35:20 +0530

    Time: 80 ms (96.11%), Space: 49.2 MB (89.38%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> curr;
+        partition(s, 0, curr, res);
+        return res;
+    }
+    
+    void partition(string &s, int index, vector<string> &curr, vector<vector<string>> &res)
+    {
+        if(index == s.size())
+        {
+            res.push_back(curr);
+            return;
+        }
+        
+        for(int i = index; i < s.size(); i++)
+        {
+            if(isPalindrome(s, index, i))
+            {
+                curr.push_back(s.substr(index, i - index + 1));
+                partition(s, i + 1, curr, res);
+                curr.pop_back();
+            }
+        }
+    }
+    
+    bool isPalindrome(string &s, int start, int end) {
+        while(start <= end)
+        {
+            if(s[start++] != s[end--])
+                return false;
+        }
+        return true;
+    }
+};
+
+//abaaba
