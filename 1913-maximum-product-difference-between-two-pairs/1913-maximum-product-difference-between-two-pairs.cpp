8f81ed7fe5ecc94b7761e2e162092fe0d1c4525a
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-06 23:17:00 +0530

    Time: 12 ms (100.00%), Space: 20.4 MB (14.82%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,7 +1,47 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
+        // return usingSort(nums);
+        return usingSearch(nums);
+    }
+    
+    //time complexity - O(nlogn)
+    int usingSort(vector<int>& nums)
+    {
         sort(nums.begin(), nums.end());
         return (nums[nums.size() - 1] * nums[nums.size() - 2]) - (nums[0] * nums[1]);
+    }
+    
+    //time complexity - O(N)
+    int usingSearch(vector<int>& nums)
+    {
+        int minlow, minhigh, maxlow, maxhigh;
+        minlow = minhigh = INT_MAX;
+        maxlow = maxhigh = INT_MIN;
+        
+        for(int i:nums)
+        {
+            if(i < minlow)
+            {
+                minhigh = minlow;
+                minlow = i;
+            }
+            else if(i < minhigh)
+            {
+                minhigh = i;
+            }
+            
+            if(i > maxhigh)
+            {
+                maxlow = maxhigh;
+                maxhigh = i;
+            }
+            else if(i > maxlow)
+            {
+                maxlow = i;
+            }
+        }
+
+        return ((maxhigh * maxlow) - (minlow * minhigh));
     }
 };
