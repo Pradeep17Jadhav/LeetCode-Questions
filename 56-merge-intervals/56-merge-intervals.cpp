70cbd7ce7dc672d95d8fe18db7da8362b7ba208a
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-08 20:28:07 +0530

    Time: 37 ms (20.47%), Space: 19 MB (17.71%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -8,13 +8,9 @@ public:
         for(int i = 1; i < intervals.size(); i++)
         {
             if(newIntervals.back()[1] >= intervals[i][0])
-            {
                 newIntervals.back()[1] = max(newIntervals.back()[1], intervals[i][1]);
-            }
             else
-            {
                 newIntervals.push_back(intervals[i]);
-            }
         }
         return newIntervals;
     }
