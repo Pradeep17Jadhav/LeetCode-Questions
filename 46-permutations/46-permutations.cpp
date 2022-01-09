e61b7a795ea712f887a420e0edc7bb4a5e1c7890
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-09 21:01:09 +0530

    Time: 6 ms (23.03%), Space: 7.4 MB (98.88%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> curr = nums;
+        int n = nums.size();
+
+        int fact = 1;
+        for(int i = n; i > 0; i--)
+        {
+            fact *= i;
+        }
+        
+        for(int x = 0; x < fact; x++)
+        {
+            res.push_back(curr);
+            if(res.size() == fact)
+            {
+                break;
+            }
+
+            int breakdown = -1;
+            for(int i = n - 1; i > 0; i--)
+            {
+                if(curr[i - 1] < curr[i])
+                {
+                    breakdown = i - 1;
+                    break;
+                }
+            }
+
+            for(int i = n - 1; i >= 0; i--)
+            {
+                if(curr[i] > curr[breakdown])
+                {
+                    swap(curr[i], curr[breakdown]);
+                    reverse(curr.begin() + breakdown + 1, curr.end());
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
