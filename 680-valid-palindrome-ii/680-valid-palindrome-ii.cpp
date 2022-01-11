a4f02ecdcee3e895986adfeb2347c0107929fcd1
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-11 22:22:05 +0530

    Time: 83 ms (14.02%), Space: 19.6 MB (67.56%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        int bonus = 1;
+        // return validPalindromeSimple(s);
+        return isPalindromeRecursive(s, 0, s.size() - 1, 0, 1);
+    }
+    
+    bool isPalindromeRecursive(string &s, int front, int back, int deleteCount, int deleteAllowedCount)
+    {
+        if(deleteCount > deleteAllowedCount)
+            return false;
+
+        while(front < back)
+        {
+            if(s[front] == s[back])
+            {
+                front++;
+                back--;
+            }
+            else
+            {
+                return (isPalindromeRecursive(s, front + 1, back, deleteCount + 1, deleteAllowedCount) or 
+                        isPalindromeRecursive(s, front, back - 1, deleteCount + 1, deleteAllowedCount));
+            }
+        }
+        return true;
+    }
+    
+    //Simple approach without recursion
+    //cannot be generalised for K number delete allowed
+    bool validPalindromeSimple(string &s)
+    {
         int n = s.size();
         int front1 = 0;
         int back1 = s.size() - 1;
