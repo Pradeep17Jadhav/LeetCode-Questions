ec49331a5ddc7b4b6f6d05bcf5e91a42e8115e0c
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-11 20:18:59 +0530

    Time: 4 ms (92.23%), Space: 7.3 MB (89.27%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str;
-        for(auto i:s)
-        {
-            if(isalnum(i))
-            {
-                if(isupper(i))
-                    str += i + 32;
-                else
-                    str += i;
-            }
+//         string str;
+//         for(auto i:s)
+//         {
+//             if(isalnum(i))
+//             {
+//                 if(isupper(i))
+//                     str += i + 32;
+//                 else
+//                     str += i;
+//             }
                 
-        }
+//         }
         
-        int n = str.size();
-        for(int i = 0; i < n / 2; i++)
+//         int n = str.size();
+//         for(int i = 0; i < n / 2; i++)
+//         {
+//             if(str[i] != str[n - i - 1])
+//                 return false;
+//         }
+//         return true;
+        
+        int front = 0;
+        int back = s.size() - 1;
+        while(front < back)
         {
-            if(str[i] != str[n - i - 1])
-                return false;
+            if(!isalnum(s[front]))
+            {
+                front++;
+            }
+            else if(!isalnum(s[back]))
+            {
+                back--;
+            }
+            else
+            {
+                if(tolower(s[front++]) != tolower(s[back--]))
+                    return false;
+            }
         }
         return true;
     }
