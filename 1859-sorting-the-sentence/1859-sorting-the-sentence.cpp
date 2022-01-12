788b0108c80b6b9f7aa805e9f6c597cce5aa447c
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-12 22:29:55 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (70.47%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     string sortSentence(string s) {
-
         string res;
-        int start = 0;
-        map<int, string> hash;
+        
+        //to save starting index of current word
+        int start = 0; 
+        map<int, string> m;
+
         for(int i = 1; i < s.size(); i++)
         {
+            //keep loop running till we find a number
             if(isdigit(s[i]))
             {
-                hash[s[i]] = s.substr(start, i - start);
-                start = i + 2;
+                //add substring from s[start] to s[i-1] index in map with key s[i]
+                m[s[i]] = s.substr(start, i - start);
+                
+                //skip next char as it will be a space
                 i++;
+                
+                //update start index for next word
+                start = i + 1;
             }
         }
         
-        for(auto i:hash)
+        //since map is sorted, we will concat the values for keys 1,2,3,....,n
+        for(auto i:m)
         {
-                res += i.second + " ";
+            res += i.second + " ";
         }
-       return res.substr(0, res.size() - 1);
+        
+        //remove the space after last word and return
+        return res.substr(0, res.size() - 1);
     }
 };
