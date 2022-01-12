9790df138844c14ff6948264bf2e2bb4ff025a41
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-12 23:13:02 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (37.40%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,6 +1,46 @@
 class Solution {
 public:
     string sortSentence(string s) {
+        // return sortSentenceUsingMap(s);
+        return sortSentenceUsingVector(s);
+    }
+    
+    string sortSentenceUsingVector(string &s)
+    {
+        //since numbers of words are from 1 to 9
+        vector<string> res(9);
+        
+        //to save starting index of current word
+        int start = 0; 
+        
+        for(int i = 1; i < s.size(); i++)
+        {
+            //keep loop running till we find a number
+            if(isdigit(s[i]))
+            {
+                //add substring from s[start] to s[i-1] index in map with key s[i]
+                res[s[i] - '0' - 1] = s.substr(start, i - start);
+                
+                //skip next char as it will be a space
+                i++;
+                
+                //update start index for next word
+                start = i + 1;
+            }
+        }
+        
+        string ans = "";
+        for(string i:res)
+        {
+            if(i.size())
+                ans += i + " ";
+        }
+        ans.pop_back();
+        return ans;
+    }
+        
+    string sortSentenceUsingMap(string &s)
+    {
         string res;
         
         //to save starting index of current word
