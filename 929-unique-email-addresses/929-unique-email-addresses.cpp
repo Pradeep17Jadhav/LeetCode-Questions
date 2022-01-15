b4ec23f57c5eea6534234fefa21cafea3ac02ce3
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-15 20:46:46 +0530

    Time: 25 ms (60.75%), Space: 13.3 MB (93.52%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.cpp
+++ 929-unique-email-addresses/929-unique-email-addresses.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
-        set<string> correct;
+        unordered_set<string> correct;
         string curr = "";
         bool valid = true;
-        for(string i:emails)
+        for(string &i:emails)
         {
             for(int j = 0; j < i.size(); j++)
             {
                 if(i[j] == '@')
                 {
-                    correct.insert(curr + '@' + i.substr(j + 1));
+                    correct.insert(curr + i.substr(j));
                     break;
                 }
                 
@@ -25,8 +25,6 @@ public:
             valid = true;
         }
         
-        for(string i : correct)
-            cout << i << endl;
         return correct.size();
     }
 };
