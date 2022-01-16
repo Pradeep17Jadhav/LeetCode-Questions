70f5312150dc02c19e57c4befced95ab3f17a2a1
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-16 19:34:38 +0530

    Time: 7 ms (63.23%), Space: 7.3 MB (36.77%) - LeetHub

--- 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.cpp
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.cpp
@@ -17,19 +17,25 @@ public:
         int n = s.size();
         for(int i = 0; i < n; i++)
         {
+            //ignore alphabets
             if(isalpha(s[i])) 
                 continue;
             
+            //digits not allowed
             else if(isdigit(s[i])) 
                 return false;
             
             else if(s[i] == '-')
             {
+                //update count of hyphens
                 hyphenCount++;
+                
+                //not valid if more than 1 hyphen, hyphen at start/end, not surrounded by alphabets
                 if(hyphenCount > 1 or i == 0 or i == n-1 or !isalpha(s[i-1]) or !isalpha(s[i+1]))
                     return false;
                 
             }
+            //all punctuations should be last character
             else if(i != n-1)
                 return false;
         }
