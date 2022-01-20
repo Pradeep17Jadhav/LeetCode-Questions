f2def13d8666d7e3eafda23f442f31ea765b6ff5
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-20 22:02:16 +0530

    Time: 240 ms (26.79%), Space: 57.5 MB (67.57%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -1,41 +1,82 @@
 class BrowserHistory {
-    stack<string>prev; // store previous urls
-    stack<string>next; // store next urls
-    string curr; //store current url
+    int i = 0;
+    vector<string> browser;
 public:
     BrowserHistory(string homepage) {
-        curr = homepage;
+        i++;
+        browser.push_back(homepage);
     }
     
-    //push curr url in prev stack, update curr url, empty front stack
+
     void visit(string url) {
-        prev.push(curr);
-        curr = url;
-        next = stack<string>(); //empty front stack
+        i++;
+        browser.resize(i);
+        browser[i - 1] = url;
     }
     
     //push curr urls to next stack, make curr url as top of prev stack
     string back(int steps) {
-        while(steps-- and !prev.empty())
+        if(steps >= i)
         {
-            next.push(curr);
-            curr = prev.top();
-            prev.pop();
+            i = 1;
+            return browser[0];
         }
-        return curr;
+        i -= steps;
+        return browser[i - 1]; //0 1 2 3 i=4 5 6
     }
     
     //push curr url in next stack, make curr url as top of next stack
     string forward(int steps) {
-        while(steps-- and !next.empty())
+        if(i + steps > browser.size())
         {
-            prev.push(curr);
-            curr = next.top();
-            next.pop();
+            i = browser.size();
+            return browser[i - 1];
         }
-        return curr;
+        
+        i += steps;
+        return browser[i - 1];
     }
 };
+
+// class BrowserHistory {
+//     stack<string>prev; // store previous urls
+//     stack<string>next; // store next urls
+//     string curr; //store current url
+// public:
+//     BrowserHistory(string homepage) {
+//         curr = homepage;
+//     }
+    
+//     //push curr url in prev stack, update curr url, empty front stack
+//     void visit(string url) {
+//         prev.push(curr);
+//         curr = url;
+//         next = stack<string>(); //empty front stack
+//     }
+    
+//     //push curr urls to next stack, make curr url as top of prev stack
+//     string back(int steps) {
+//         while(steps-- and !prev.empty())
+//         {
+//             next.push(curr);
+//             curr = prev.top();
+//             prev.pop();
+//         }
+//         return curr;
+//     }
+    
+//     //push curr url in next stack, make curr url as top of next stack
+//     string forward(int steps) {
+//         while(steps-- and !next.empty())
+//         {
+//             prev.push(curr);
+//             curr = next.top();
+//             next.pop();
+//         }
+//         return curr;
+//     }
+// };
+
 
 /**
  * Your BrowserHistory object will be instantiated and called as such:
