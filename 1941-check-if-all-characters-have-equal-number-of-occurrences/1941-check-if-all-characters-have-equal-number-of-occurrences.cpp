5ac39cebecdecd190583a823a2c72303d8761ffc
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-24 23:38:23 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (95.83%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        // return usingMap(s);
+        return usingVector(s);
+    }
+
+    //vector uses constant time for accessing element
+    bool usingVector(string &s)
+    {
+        vector<int>hash(26);
+        for(char &c:s) 
+            hash[c - 'a']++;
+
+        for(int &i: hash)
+        {
+            if(i and i != hash[s[0] - 'a']) 
+                return false;
+        }
+
+        return true;
+    }
+
+    //map uses logarithmic time for accessing element
+    bool usingMap(string &s)
+    {
+        unordered_map<char, int>hash;
+        for(char &c:s) hash[c]++;
+        for(auto &[k, v]:hash)
+            if(v != hash[s[0]]) return false;
+        return true;
+    }
+};
