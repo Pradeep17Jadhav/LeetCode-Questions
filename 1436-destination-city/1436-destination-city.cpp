aa93b03e4d325e1c537ccab9dacd1fb2aab8236e
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-24 23:11:40 +0530

    Time: 17 ms (36.55%), Space: 10.7 MB (80.78%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        return destCity1(paths);
-        // return destCity2(paths);
+        // return destCity1(paths);
+        return destCity2(paths);
     }
     
     string destCity1(vector<vector<string>>& paths) {
@@ -19,12 +19,12 @@ public:
         unordered_map<string, int>cities;
         for(auto &path: paths)
         {
-            cities[path[0]] += 2;
-            cities[path[1]] += 1;
+            cities[path[0]] += 1;
+            cities[path[1]] += 0;
         }
         
         for(auto &[k, v]: cities)
-            if(v == 1) return k;
+            if(v == 0) return k;
         return "";
     }
 };
