f0607db874b7a3e9a47026933df1f70f12d806f5
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-26 14:52:27 +0530

    Time: 4 ms (74.39%), Space: 6.7 MB (59.76%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int secondHighest(string s) {
+        // return usingHashVector(s);
+        return withoutExtraSpace(s);
+    }
+    
+    //Time - O(N), Space - O(1)
+    int withoutExtraSpace(string &s)
+    {
+        int last = -1;
+        int secondLast = -1;
+        
+        for(char &c:s)
+        {
+            if(c >= '0' and c <= '9')
+            {
+                if(last < c - '0')
+                {
+                    secondLast = last;
+                    last = c - '0';
+                }
+                else if(last >  c - '0' and secondLast < c - '0')
+                    secondLast = c - '0';
+            }
+        }
+        return secondLast;
+    }
+
+    //Time - O(N + 10), Space - O(1)
+    int usingHashVector(string &s)
+    {
         vector<bool>hash(10, false);
         for(char &c:s)
         {
