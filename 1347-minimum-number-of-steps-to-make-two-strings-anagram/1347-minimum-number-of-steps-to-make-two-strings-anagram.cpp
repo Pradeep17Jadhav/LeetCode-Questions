f3751bf8082077205d0992f48d9b5042d1429f03
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-01-29 21:20:24 +0530

    Time: 60 ms (73.40%), Space: 16.6 MB (39.69%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
+        // return minStepsMethod1(s, t);
+        return minStepsMethod2(s, t);
+    }
+    
+    int minStepsMethod2(string &s, string &t)
+    {
+        vector<int> hash(26);
+        for(int i = 0; i < s.size(); i++)
+        {
+            hash[s[i] - 'a']++;
+            hash[t[i] - 'a']--;
+        }
+            
+        int res = 0;
+        for(int &i: hash)
+        {
+            if(i > 0)
+                res += i;
+        }
+        
+        return res;
+    }
+    
+    int minStepsMethod1(string &s, string &t) {
         vector<int> hash(26);
         for(char &c: s)
             hash[c - 'a']++;
