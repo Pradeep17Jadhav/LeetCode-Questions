47bf573a5d47c357f6074a5a289c4a1f03cca413
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-02-08 20:49:02 +0530

    Time: 6 ms (14.93%), Space: 5.8 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int addDigits(int num) {
+        return method1(num);
+        // return method2(num);
+    }
+        
+    int method1(int &num)
+    {
+        // if(!num) return 0;
+        // return num % 9 == 0 ? 9 : num % 9;
+        // edge case for 0 can be handled using next method
+        // subtract 1 from original num, so remainder of 0 becomes 8
+        // then add 1 in result. this trick works correctly for all nos.
+        return (num - 1) % 9 + 1; 
+    }
+
+    int method2(int &num)
+    {
         int sum = num;
         while(sum > 9)
         {
