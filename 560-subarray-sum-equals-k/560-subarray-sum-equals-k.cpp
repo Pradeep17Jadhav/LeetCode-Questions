e7b567cdf79909a58de98e021a9a0f0e1594d4c8
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-02-10 22:33:00 +0530

    Time: 109 ms (53.98%), Space: 36.1 MB (87.06%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     //reference - https://www.youtube.com/watch?v=HbbYPQc-Oo4
+    //TC: O(N^2)
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>hash;
         int res = 0;
-        int cumm = 0;
+        int cumm = 0; //to store cummilative/prefix sum
         int i = 0;
+        hash[0]++; // to count subarrays which starts at index 0;
         while(i < nums.size())
         {
             cumm += nums[i];
-            if(cumm == k)
-                res++;
-            
-            res += hash[cumm - k];
+            if(hash.find(cumm - k) != hash.end()) //can work without this but also, but space complexity will increase
+                res += hash[cumm - k];
             hash[cumm]++;
             i++;
         }
         return res;
     } 
 };
+
+//try this test case:
+//[3,4,7,2,-3,1,4,2]
+//k = 7
