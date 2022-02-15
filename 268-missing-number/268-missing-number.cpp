2b441cae15882e56aeb6a09fea2be1278ce20f02
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-02-15 19:11:22 +0530

    Time: 23 ms (62.99%), Space: 18 MB (64.90%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        return method1(nums);
+        // return method2(nums);
+    }
+    
+    //since a^b^b = a,
+    //we can use xor to find missing number
+    int method1(vector<int>& nums)
+    {
+        int res = nums.size();
+        for(int i = 0; i < nums.size(); i++)
+        {
+            res = res ^ i ^ nums[i];
+        }
+        return res;
+    }
+
+    int method2(vector<int>& nums) {
         int indexSum = 0;
         int valueSum = 0;
         for(int i = 0; i < nums.size(); i++)
