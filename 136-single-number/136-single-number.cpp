e51daa05d0cdedd6886b8ffd101ac4cd4f50f71b
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-02-15 18:31:29 +0530

    Time: 18 ms (79.59%), Space: 16.9 MB (50.83%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        return method1(nums);
+        // return method2(nums);
+    }
+        
+    //TC: O(n) using XOR
+    int method1(vector<int>& nums)
+    {
+        int res = 0;
+        for(int &i: nums)
+            res ^= i; //xor will cancel out the same numbers
+        return res;
+    }
+
+    //TC: O(nlogn)
+    int method2(vector<int>& nums)
+    {
         sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size() - 1; i+=2)
         {
