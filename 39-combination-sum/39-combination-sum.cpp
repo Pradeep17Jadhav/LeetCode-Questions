72cf9eef522665da7bba71ebc46e9b48037344ab
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-02-17 23:47:47 +0530

    Time: 14 ms (50.55%), Space: 17 MB (33.76%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> res; // 2D vector to store answer
+    void solve(int i, vector<int>& arr, vector<int>& temp, int target)
+    {
+        // if target becomes zero at any point, we have find a possible combination
+        if(target == 0) 
+        {
+            res.push_back(temp);
+            return;
+        }
+
+        // if target becomes < 0 or >size, then it is not possible to have ans
+        if(target < 0 || i == arr.size())
+            return;
+
+        
+        //for every element we have two choices either to include in answer or not to include in answer. 
+        //first we do not take the ith element
+        //so without decreasing sum we will move to next index because it will not contribute in making our sum
+        solve(i + 1, arr, temp, target);
+        
+        // we are taking the ith element and not moving onto the next element because
+        //it may be possible that this element again contribute in making our sum. 
+        temp.push_back(arr[i]); // including ith element
+        
+        //we should decrease our target sum as we are considering that this will help us in
+        //making our target sum, and call again function
+        solve(i, arr, temp, target - arr[i]); 
+        temp.pop_back(); // backtrack
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        res.clear(); // clear global array, make to sure that no garbage value is present in it
+        
+        vector<int> temp; // temporary vector that tries all possible combination
+        
+        solve(0, arr, temp, target); // calling function, and see we start from index zero
+        
+        return res; // finally return the answer array
+    }
+};
