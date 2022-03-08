9f76b96e437dbdd7d3ccb0b448e40b9c2959f79d
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-03-08 21:12:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (63.29%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
     bool isHappy(int n) {
+        // return solutionHash(n);
+        return solutionFloyedCycleDetection(n);
+    }
+    
+    //find next sum of square of all digits
+    int next(int n)
+    {
+        int sum = 0;
+        int rem;
+        while(n)
+        {
+            rem = n % 10;
+            sum += rem * rem;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    //floyed cycle detection algorithm
+    bool solutionFloyedCycleDetection(int &n) {
+        int fast = n;
+        int slow = n;
+
+        do {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        while(slow != fast);
+
+        return fast == 1;
+    }
+    
+    //using hash to store visited value. if value is repeated then its a cycle.
+    bool solutionHash(int &n) {
         int sum = n;
         int rem;
         unordered_set<int>hash;
         while(sum != 1)
         {
-            int curr = 0;
-            while(sum)
-            {
-                rem = sum % 10;
-                curr += rem * rem;
-                sum /= 10;
-            }
-            
-            sum = curr;
+            sum = next(sum);
+
+            //check if value is repeated. break if repeated as it will then go in a loop.
             if(hash.find(sum) != hash.end())
                 break;
             
             hash.insert(sum);
         }
-        
         return sum == 1;
     }
 };
