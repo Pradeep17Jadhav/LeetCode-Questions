f0e336f77de2ab81f82f7b07c19724e4a54b344e
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-03-10 22:54:40 +0530

    Time: 76 ms (14.20%), Space: 70.7 MB (99.54%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* a, ListNode* b) {
+        ListNode* head = a;
+        ListNode* prev = a;
+        int carry = 0;
+        int sum = 0;
+        
+        while(a || b)
+        {
+            //sum = carry + a + b
+            sum = carry;
+            if(a) sum += a->val;
+            if(b) sum += b->val;
+            
+            carry = sum / 10; //update carry
+            a->val = sum % 10; //use "a" list to update ans in-place
+
+            //if list "b" has more nodes than list "a",
+            //point the list "a" to remaining nodes of list "b"
+            //so, we can use any list (a or b) to update answer in-place
+            if(b && b->next && !a->next)
+            {
+                a->next = b->next;
+                b->next = nullptr;
+            }
+
+            prev = a; //keep track of last node of list "a"
+            
+            //move to next nodes
+            if(a) a = a->next;
+            if(b) b = b->next;
+
+        }
+        
+        //add new node for carry at the end
+        if(carry)
+            prev->next = new ListNode(carry, nullptr);
+
+        return head;
+    }
+};
