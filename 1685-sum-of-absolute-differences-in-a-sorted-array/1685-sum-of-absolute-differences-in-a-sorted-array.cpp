09226b2122bdaa24d9ba67380fa95e6076ba32ad
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-03-24 22:44:31 +0530

    Time: 232 ms (17.31%), Space: 87.4 MB (41.07%) - LeetHub

---
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int sum = 0;
+        vector<int>res;
+        for(int &i: nums) //first find the total sum of all the elements in array
+            sum += i;
+        
+        //Formula:: val = (elementsBefore * nums[i] - sumBefore) + 0 + (sumAfter - elementsAfter * nums[i]);
+        //---------------------------------------------------------------------
+        //absolute diff with current element is always 0 so it can be ignored
+        //Suppose there are 4 elements before current element. Then we will have to compare all of them with
+        //current element, so we compare 'elementsBefore' * nums[i], i.e.
+        //let [1 4 6 8 10], and current is 2nd index i.e. 6.
+        //then summation of absolute differences on left of 6 = (6-1)+(6-4) = 6*2+(-1-4) = 7
+        //this can be written as = (6+6+...+6)-(1+4+...+x)
+        // = (elementsBefore * nums[i]) - (sum of elements before current element)
+        // = (elementsBefore * nums[i] - sumBefore)
+        
+        //Similarly we can use similar approach to find the summation of absolute differences on right side
+        //let [1 4 6 8 10], and current is 2nd index i.e. 6.
+        //summation of absolute differences on right of 6 = (8-6)+(10-6) = (8+10)-6*2 = 6
+        // = (8+10+...+x) - (6+6+...+6)
+        // = (sum of elements after current element) - (elementsAfter * nums[i])
+        // = (sumAfter - elementsAfter * nums[i])
+        
+        
+        int sumBefore = 0; //sum of all elements before the current element
+        int sumAfter = sum; //sum of all elements after the current element till the end
+        for(int i = 0; i < nums.size(); i++)
+        {
+            sumAfter -= nums[i];
+            int elementsBefore = i;
+            int elementsAfter = nums.size() - i - 1;
+            int val = (sumAfter - elementsAfter * nums[i]) + (elementsBefore * nums[i] - sumBefore);
+            res.push_back(val);
+            sumBefore += nums[i];
+        }
+        return res;
+            
+    }
+};
