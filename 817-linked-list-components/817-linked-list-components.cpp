1b7bed734aed11e369f3ec99c3d07eb52c8e0f76
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-03-29 22:09:24 +0530

    Time: 68 ms (35.10%), Space: 21.4 MB (62.53%) - LeetHub

--- 817-linked-list-components/817-linked-list-components.cpp
+++ 817-linked-list-components/817-linked-list-components.cpp
@@ -11,6 +11,28 @@
 class Solution {
 public:
     int numComponents(ListNode* head, vector<int>& nums) {
+        return solution1(head, nums);
+        // return solution2(head, nums);
+
+    }
+    
+    int solution1(ListNode* head, vector<int>& nums) {
+        //set is used to store values of nums as it has same time complexity for find()
+        unordered_set<int> list(nums.begin(), nums.end());
+        int res = 0;
+        while(head)
+        {
+            //check if current value is in list
+            //if yes, check if it is the last node or next node is in list or not
+            //if next node is not available or not in list, it means the chain is completed, so res++
+            if(list.count(head->val) && (!head->next || !list.count(head->next->val)))
+                res++;
+            head = head->next;
+        }
+        return res;
+    }
+    
+    int solution2(ListNode* head, vector<int>& nums) {
         unordered_map<int, bool> hash;
         //maintain hash for each number we want to find
         for(int &i: nums)
