17eec9f829b93e9b8933fbfcf3659fea29384ca1
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-03-31 23:33:11 +0530

    Time: 43 ms (40.83%), Space: 13.6 MB (64.47%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        //The answer is, just return the maximum digit in the string
+        // 13524 <- this is an input
+        // 11111
+        // 01111
+        // 01101
+        // 00101
+        // 00100
+        //so the maximum digit decides the answer
+
+        return minPartitionsOneLiner(n);
+        // return minPartitionsManually(n);
+    }
+
+    int minPartitionsOneLiner(string &n) {
+        return *max_element(begin(n), end(n)) - '0';
+    }
+    int minPartitionsManually(string &n) {
+        int maxx = -1; //as number is +ve integer
+        for(char &c: n)
+            maxx = max(maxx, c-'0');
+        return maxx;
+    }
+};
