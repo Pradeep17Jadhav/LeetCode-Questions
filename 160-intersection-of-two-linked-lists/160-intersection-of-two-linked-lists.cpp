8589d4d3c1c4b9eeb0bccc948834703abe9ec4bb
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-02 23:37:37 +0530

    Time: 54 ms (68.23%), Space: 14.7 MB (32.96%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -11,19 +11,44 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         return getIntersectionNodeSolution1(headA, headB);
         // return getIntersectionNodeSolution2(headA, headB);
+        // return getIntersectionNodeSolution3(headA, headB);
+    }
+
+    //TC: O(2m+2n) = O(m+n)
+    //SC: O(1)
+    ListNode *getIntersectionNodeSolution1(ListNode *A, ListNode *B) {
+        ListNode* ptrA = A;
+        ListNode* ptrB = B;
+        
+        while(ptrA != ptrB) //this will terminate when intersection is found or both lists become null 
+        {
+            //when either of list ends, start it again with head of other list
+            //so on 2nd iteration of both lists, the will reach intersection at same time
+            //if there is no intersection, both will reach NULL at same time
+            //logic: Even if m>n or n>m, if we iterate m+n for both lists, len(m+n) == len(n+m)
+            ptrA = ptrA ? ptrA->next : B;
+            ptrB = ptrB ? ptrB->next : A;
+        }
+        return ptrA; //or return ptrB;
     }
     
-    ListNode *getIntersectionNodeSolution1(ListNode *A, ListNode *B) {
+    //TC: O(2m+2n) = O(m+n)
+    //SC: O(1)
+    ListNode *getIntersectionNodeSolution2(ListNode *A, ListNode *B) {
         int countA = 0;
         int countB = 0;
         ListNode* ptrA = A;
         ListNode* ptrB = B;
         
+        //count the elements in each lists
         while(ptrA)
             countA++, ptrA = ptrA->next;
         while(ptrB)
             countB++, ptrB = ptrB->next;
         
+        //if both list has unequal elements,  skip the 'diff' nodes from bigger list
+        //since both the interected list should end at same point, the extra starting nodes of bigger list 
+        //definately does not have the intersection so we just skip them
         if(countA > countB)
         {
             int diff = countA - countB;
@@ -36,6 +61,8 @@ public:
             while(diff--)
                 B = B->next;
         }
+        
+        //since both lists now have equal elements, we can find intersection by traversing them both simultaneously
         while(A && B)
         {
             if(A == B)
@@ -46,22 +73,24 @@ public:
         return NULL;
     }
     
-    ListNode *getIntersectionNodeSolution2(ListNode *headA, ListNode *headB) {
+    //TC: O(m+n)
+    //SC: O(m+n)
+    ListNode *getIntersectionNodeSolution3(ListNode *A, ListNode *B) {
         unordered_set<ListNode*> hash;
-        while(headA || headB)
+        while(A || B)
         {
-            if(headA)
+            if(A)
             {
-                if(hash.count(headA)) return headA;
-                hash.insert(headA);
-                headA = headA->next;
+                if(hash.count(A)) return A;
+                hash.insert(A);
+                A = A->next;
             }
 
-            if(headB)
+            if(B)
             {
-                if(hash.count(headB)) return headB;
-                hash.insert(headB);
-                headB = headB->next;
+                if(hash.count(B)) return B;
+                hash.insert(B);
+                B = B->next;
             }
         }
         return NULL;
