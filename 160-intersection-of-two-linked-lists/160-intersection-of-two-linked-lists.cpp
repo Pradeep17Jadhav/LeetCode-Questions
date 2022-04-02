0b856ed91f4d7faea9e98f2aee06986e69291fc6
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-02 23:11:04 +0530

    Time: 55 ms (65.97%), Space: 14.6 MB (32.96%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,6 +9,44 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        return getIntersectionNodeSolution1(headA, headB);
+        // return getIntersectionNodeSolution2(headA, headB);
+    }
+    
+    ListNode *getIntersectionNodeSolution1(ListNode *A, ListNode *B) {
+        int countA = 0;
+        int countB = 0;
+        ListNode* ptrA = A;
+        ListNode* ptrB = B;
+        
+        while(ptrA)
+            countA++, ptrA = ptrA->next;
+        while(ptrB)
+            countB++, ptrB = ptrB->next;
+        
+        if(countA > countB)
+        {
+            int diff = countA - countB;
+            while(diff--)
+                A = A->next;
+        }
+        if(countB > countA)
+        {
+            int diff = countB - countA;
+            while(diff--)
+                B = B->next;
+        }
+        while(A && B)
+        {
+            if(A == B)
+                return A;
+            A = A->next;
+            B = B->next;
+        }
+        return NULL;
+    }
+    
+    ListNode *getIntersectionNodeSolution2(ListNode *headA, ListNode *headB) {
         unordered_set<ListNode*> hash;
         while(headA || headB)
         {
