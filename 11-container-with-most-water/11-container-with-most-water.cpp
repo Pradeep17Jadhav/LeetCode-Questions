8ebe9eb8dd6a6630a488e288492b21e79775536d
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-05 20:56:36 +0530

    Time: 142 ms (31.71%), Space: 59 MB (38.72%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,11 +1,43 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        return maxAreaSolution1(height);
+        // return maxAreaSolution2(height);
+
+    }
+    
+    //TC: O(N) - two pointers. Similar to solution 1, but concise;
+    int maxAreaSolution1(vector<int>& height) {
+        int i = 0; //left ptr
+        int j = height.size() - 1; //right ptr
+        int res = 0;
+        int minHeight; //height of shorter line between two
+
+        
+        //we will start from lines at extreme ends to consider the whole container.
+        //then we check the inner lines having height higher than current line.
+        //the inner container can have higher area IFF its height is considerably more as the width is always less.
+        //so we check for all the inner lines which have higher height than current line
+        while(i < j)
+        {
+            minHeight = min(height[i], height[j]);
+            res = max(res, minHeight * (j-i));
+            
+            while(height[i] <= minHeight && i < j)
+                i++;
+            while(height[j] <= minHeight && i < j)
+                j--;
+        }
+        return res;
+    }
+
+    //TC: O(N) - four pointers are used. similar to solution 1.
+    int maxAreaSolution2(vector<int>& height) {
         int left = 0;
         int right = height.size() - 1;
         int i = left;
         int j = right;
-        int maxx = min(height[i], height[j]) * (j-i);
+        int res = min(height[i], height[j]) * (j-i);
         
         while(i < j)
         {
@@ -24,8 +56,8 @@ public:
                 left++;
                 i++;
             }
-            maxx = max(maxx, min(height[left], height[right]) * (j-i));
+            res = max(res, min(height[left], height[right]) * (j-i));
         }
-        return maxx;
+        return res;
     }
 };
