bffe4b8555bd675935375fa4b60a1d2db7ba8af5
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-09 17:24:34 +0530

    Time: 15 ms (86.09%), Space: 13.6 MB (71.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,12 +1,47 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        // return topKFrequentSolution1(nums, k);
+        return topKFrequentSolution2(nums, k);
+    }
+
+    vector<int> topKFrequentSolution1(vector<int>& nums, int k) {
+        unordered_map<int, int> hash;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; //maxheap pq having pair of value-key
+        vector<int> res;
+
+        //add all element frequencies to hash
+        for(int &i: nums)
+            hash[i]++;
+
+        //add all frequencies in maxheap priority q and always keep only keep k elements in queue whose freq is maximum
+        for(auto kv: hash)
+        {
+            pq.push({kv.second, kv.first}); //keep frequency as first argument so the pq will be sorted as per max freq.
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        //for all remaining values in priority queue, add their keys to result array
+        while(pq.size())
+        {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        return res;
+    }
+
+    vector<int> topKFrequentSolution2(vector<int>& nums, int k) {        
         unordered_map<int, int> hash;
         priority_queue<int, vector<int>, greater<int>> pq;
         vector<int> res;
+        
+        //add all element frequencies to hash
         for(int &i: nums)
             hash[i]++;
         
+        //add all frequencies in maxheap priority q and always keep only keep k elements in queue whose freq is maximum
         for(auto kv: hash)
         {
             pq.push(kv.second);
@@ -14,6 +49,8 @@ public:
                 pq.pop();
         }
         
+        //for all hash values, check the values whose freq is greater than or equal to top value of priority queue
+        //since its a maxheap, all the values in pq are the required frequencies so add their keys to result.
         for(auto kv: hash)
         {
             if(kv.second >= pq.top())
