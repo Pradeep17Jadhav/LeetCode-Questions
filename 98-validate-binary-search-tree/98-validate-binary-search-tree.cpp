3aa5fa698680b7d97214b0cdec9c9f840e2b43a6
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-10 17:16:41 +0530

    Time: 18 ms (46.83%), Space: 21.6 MB (88.92%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBSTSubtree(root, NULL, NULL);
+    }
+    
+    bool isValidBSTSubtree(TreeNode* root, TreeNode* min, TreeNode* max) {
+        if(!root) //null subtree is valid for leaf nodes
+            return true;
+        
+        //if min node is present, the value of all the subtree nodes shoud be greater than the value of min node
+        //if max node is present, the value of all the subtree nodes shoud be less than the value of max node
+        //if not, the subtree is invalid so returen false
+        if((min && root->val <= min->val) || (max && root->val >= max->val))
+            return false;
+        
+        //both the left and rightsubtree should also be valid
+        return isValidBSTSubtree(root->left, min, root) && isValidBSTSubtree(root->right, root, max);
+        
+    }
+};
