37080b364721609dbefab7ee11b66e25bc9cceb1
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-12 23:39:10 +0530

    Time: 8 ms (95.39%), Space: 10.8 MB (44.41%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        unordered_map<int, int> seen;
+        unordered_map<int, int> seen; //to store indices of seen numbers
         int num, required;
         for(int i = 0; i < nums.size(); i++)
         {
-            num = nums[i];
-            required = target-num;
-            if(seen.count(required))
+            required = target - nums[i]; //the number required to make 2sum==target
+            if(seen.find(required) != seen.end()) //if target is already seen, return its index and index of curr number
                 return {i, seen[required]};
-            seen[num] = i;
+            seen[nums[i]] = i; //store index of current number as seen element
         }
         return {};
     }
