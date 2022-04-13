33f5f683b37431223c85a3dea1bc7faa1700220a
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-13 20:21:40 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (18.13%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>> generateMatrix(int n) {
+    vector<vector<int>> generateMatrix(int n) {  
+        return generateMatrixSolution1(n);
+        // return generateMatrixSolution2(n);
+    }
+        
+    vector<vector<int>> generateMatrixSolution1(int n) {
+        int rs = 0; //row start
+        int re = n-1; //row end
+        int cs = 0; //column start
+        int ce = n-1; //column end
+        int curr = 1;
+        vector<vector<int>> res(n, vector<int>(n, 0)); //create n*n matrix with initial value 0
+
+        while(rs <= re && cs <= ce)
+        {
+            for(int j = cs; j <= ce; j++) //from column start to column end
+                res[rs][j] = curr++;
+            
+            for(int i = rs+1; i <= re; i++) //from row start to row end
+                res[i][ce] = curr++;
+            
+            for(int j = ce-1; j >= cs; j--) //from column end to column start
+                res[re][j] = curr++;
+            
+            for(int i = re-1; i > rs; i--) //from row end to row start
+                res[i][cs] = curr++;
+            
+            cs++;
+            ce--;
+            rs++;
+            re--;
+        }
+        return res;
+    }
+
+    vector<vector<int>> generateMatrixSolution2(int n) {
         if(n == 1) return {{1}};
         int curr = 1;
         int i = 0;
