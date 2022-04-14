352d2a003acd88cbeb59508973b65ed8cda1334b
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-14 21:20:52 +0530

    Time: 7 ms (74.37%), Space: 13.7 MB (51.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
+        return minCostClimbingStairs_1(cost);
+        // return minCostClimbingStairs_2(cost);
+    }
+    
+    //DP
+    //TC: O(N)
+    //SC: O(N)
+    int minCostClimbingStairs_1(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> memo(n);
+        memo[0] = cost[0];
+        memo[1] = cost[1];
+
+        for(int i = 2; i < n; i++)
+            memo[i] = cost[i] + min(memo[i-1], memo[i-2]);
+
+        return min(memo[n-1], memo[n-2]);
+    }
+
+    //TC: O(N)
+    //SC: O(1)
+    int minCostClimbingStairs_2(vector<int>& cost) {
         int a = cost[0];
         int b = cost[1];
         int c;
