baf340242a461c88b32da0d1f216e7415fcdde7c
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-14 11:57:19 +0530

    Time: 3 ms (54.05%), Space: 6.1 MB (18.49%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     int fib(int n) {
-        // return fib1(n);
-        return fib2(n);
+        return fib1(n); //avg ans
+        // return fib2(n); //best ans
+        // return fib3(n); //worst ans
     }
 
     
+    //dynamic programming - memoisation
+    //TC: O(N)
+    //SC: O(N)
+    int fib1(int n) {
+        if(n <= 1) return n;
+        vector<int> memo(n+1);
+        memo[0] = 0;
+        memo[1] = 1;
+        
+        for(int i = 2; i <= n; i++)
+            memo[i] = memo[i-1] + memo[i-2];
+        
+        return memo[n];
+    }
+
     //Bottom up approach
     //TC: O(N)
     //SC: O(1)
-    int fib1(int n) {
+    int fib2(int n) {
         if(!n) return 0;
         int fa = 0;
         int fb = 1;
@@ -26,9 +42,9 @@ public:
     }
     
     //Recursive
-    //TC: O(2^N)
+    //TC: O(2^N) - should be avoided at all costs
     //SC: O(N)
-    int fib2(int n) {
+    int fib3(int n) {
         if(n == 0)  return 0;
         if(n == 1)  return 1;
         return fib(n-1) + fib(n-2);
