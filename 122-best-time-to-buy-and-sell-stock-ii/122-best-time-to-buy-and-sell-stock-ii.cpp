8edccb7f425b3df97566600dfe0317e52dc16e9b
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-19 23:59:32 +0530

    Time: 11 ms (40.74%), Space: 13.1 MB (51.16%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // Intuition -
+    // "Buy on first day, sell it on second day, then buy it on second, sell it on third day"
+    // Above line equals "buy it on first and sell it on third day"
+    // Ex - [1,2,4,5] = Ans Profit = 4 ... [Buy for 1, sell for 5]
+    // Which eqals - [2-1]=1 + [4-2]=2 + [5-4]=1 = 1+2+1 = 4
     int maxProfit(vector<int> &prices) {
         int ret = 0;
         for (int i = 1;i < prices.size(); i++)
