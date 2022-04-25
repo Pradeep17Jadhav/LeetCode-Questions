871dc27ba9d7893c06561f3d6247bd6d1060c4e1
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-25 20:40:57 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.44%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+    //Imagine Pascal's Triangle like a pattern
+    // 1
+    // 1    1
+    // 1    2    1
+    // 1    3    3     1
+    // 1    4    6     4    1
+    // 1    5    10    10   5   1
+    
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
-        res.push_back({1});
-        if(numRows == 1)
-            return res;
-        res.push_back({1,1});
-        if(numRows == 2)
-            return res;
+        vector<vector<int>> res;       
         
-        
-        for(int i = 2; i < numRows; i++)
+        for(int i = 0; i < numRows; i++)
         {
-            vector<int> row(i+1, 1);
+            vector<int> row(i+1, 1); //create array of i+1 length, fill with 1's
+            
+            //Use previous row of res to find current row's jth element
             for(int j = 1; j < i; j++)
                 row[j] = res[i-1][j-1] + res[i-1][j];
             
