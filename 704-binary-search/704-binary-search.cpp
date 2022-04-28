540cc9f2abccf3950bf48238810036f60dd0bc7f
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-04-28 22:37:57 +0530

    Time: 45 ms (63.54%), Space: 27.7 MB (12.98%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        return binary(nums, target, 0, nums.size()-1);
+        return binary(nums, target);
+        // return binary_recursive(nums, target, 0, nums.size()-1);
     }
     
-    int binary(vector<int> nums, int target, int left, int right)
+    // TC: O(logn)
+    // SC: O(1)
+    int binary(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        while(left <= right) 
+        {
+            int mid = (right + left) / 2;
+            if(nums[mid] == target) return mid;
+            if(target < nums[mid])
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+        return -1;
+
+    }
+    
+    // TC: O(logn)
+    // SC: O(N) for recursion
+    int binary_recursive(vector<int> nums, int target, int left, int right)
     {
         if(left > right) return -1;
         int mid = (right + left) / 2;
         if(nums[mid] == target) return mid;
-        return  target < nums[mid]  ? binary(nums, target, left, mid-1) : binary(nums, target, mid+1, right);
+        return  target < nums[mid]  ? binary_recursive(nums, target, left, mid-1) : binary_recursive(nums, target, mid+1, right);
     }
 };
