b4fc2da02b6324406e0bfc7aa97cb20e1a9bc40c
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-01 19:40:12 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (39.15%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
+        return backspaceCompare_1(s, t);
+        // return backspaceCompare_2(s, t);
+    }
+
+    //TC: O(s+t) = O(N)
+    //SC: O(1)
+    bool backspaceCompare_1(string s, string t) {
+        int ptrS = 0;
+        int ptrT = 0;
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] != '#')
+                s[ptrS++] = s[i];
+            else if(ptrS)
+                ptrS--;
+        }
+        
+        for(int i = 0; i < t.size(); i++)
+        {
+            if(t[i] != '#')
+                t[ptrT++] = t[i];
+            else if(ptrT)
+                ptrT--;
+        }
+        return s.substr(0, ptrS) == t.substr(0, ptrT);
+    }
+
+    //TC: O(s+t) = O(N)
+    //SC: O(s+t) = O(N)
+    bool backspaceCompare_2(string s, string t) {
         string tempS = "";
         string tempT = "";
         for(int i = 0; i < s.size(); i++)
