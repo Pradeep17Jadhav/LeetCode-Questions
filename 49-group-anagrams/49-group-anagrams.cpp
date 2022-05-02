f06d011b74e9bcd2c2b572baf655397368dfdf93
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-02 22:39:20 +0530

    Time: 95 ms (14.09%), Space: 23 MB (18.20%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,10 +3,12 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> hash;
         vector<vector<string>> res;
+        
+        //store each string in a hash using sorted string as a key
         for(string &s: strs)
         {
             string temp = s;
-            sort(temp.begin(), temp.end());
+            sortString(temp);
             hash[temp].push_back(s);
         }
         
@@ -14,5 +16,25 @@ public:
             res.push_back(kv.second);
         
         return res;
+    }
+    
+    //Instead of using song() of STL, we can implement our own sort for this question
+    //TC: O(N) 
+    //SC: O(26) = O(1)
+    void sortString(string &s)
+    {
+        vector<int> hash(26);
+        int ptr = 0;
+        for(char &c: s)
+            hash[c - 'a']++;
+        
+        for(int i = 0; i < 26; i++)
+        {
+            if(hash[i])
+            {
+                s[ptr++] = i + 'a';
+                hash[i--]--;
+            }
+        }
     }
 };
