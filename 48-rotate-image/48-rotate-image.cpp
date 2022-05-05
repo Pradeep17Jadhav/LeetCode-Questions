45ffaefb395f0c3d69aa45a7735929c7092459dc
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-05 20:57:31 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (34.57%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -15,7 +15,8 @@ public:
 /* COMMON STRATEGY FOR ROTATING ARRAY ->
  *
  * clockwise rotate
- * first reverse up to down, then swap the symmetry 
+ * first reverse rows up to down, then swap the symmetry 
+ * (first swap symmetry and then reverse all columns left to right also works)
  * 1 2 3     7 8 9     7 4 1
  * 4 5 6  => 4 5 6  => 8 5 2
  * 7 8 9     1 2 3     9 6 3
@@ -24,6 +25,7 @@ public:
  *
  * anticlockwise rotate
  * first reverse left to right, then swap the symmetry
+ * (first swap symmentry and then reverse columns up to down also works)
  * 1 2 3     3 2 1     3 6 9
  * 4 5 6  => 6 5 4  => 2 5 8
  * 7 8 9     9 8 7     1 4 7
