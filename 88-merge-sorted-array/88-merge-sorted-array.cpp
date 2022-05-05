541087e3828f31a9f733b597780b0e454536f135
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-05 23:33:19 +0530

    Time: 4 ms (49.19%), Space: 9.1 MB (73.40%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,9 +2,36 @@ class Solution {
 public:
     // IMPORTANT - GAP Algorithm
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        GAP_Algo(nums1, m, nums2, n);
+        Start_from_end(nums1, m, nums2, n);
+        // GAP_Algo(nums1, m, nums2, n); //important
         // Naive_Merge(nums1, m, nums2, n);
     }
+    
+    //TC : O(m+n) = O(N)
+    //SC: O(1)
+    //It is guaranteed that all elements will accomodate in nums1
+    //Start iterating from end of both the arrays and compare them.
+    //Use a ptr at end of nums1 which will be used to update the whole array
+    //Just compare the ith and jth value and put the greatest value at the end using ptr
+    void Start_from_end(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int ptr = nums1.size() - 1;
+        int i = m - 1;
+        int j = n - 1;
+        
+        while(i >= 0 && j >= 0)
+        {
+            if(nums1[i] > nums2[j])
+                nums1[ptr--] = nums1[i--];
+            else
+                nums1[ptr--] = nums2[j--];
+        }
+        
+        //as we check nums1[i] > nums2[j] first, it may happen that both are equal but we just use ith value.
+        //so nums2 may still have few values left. add them into the array
+        while(j >= 0)
+            nums1[ptr--] = nums2[j--];
+    }
+
     
     // TC : O(NlogN)
     // SC : O(1)
@@ -33,6 +60,7 @@ public:
         }
     }
     
+
     //Naive Solution - Not for interviews
     void Naive_Merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         for(int i = m, j = 0; i < nums1.size(), j < n; i++, j++)
