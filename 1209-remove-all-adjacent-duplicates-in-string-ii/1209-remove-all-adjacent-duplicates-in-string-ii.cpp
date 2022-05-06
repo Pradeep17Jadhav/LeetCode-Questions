876da551187603c1fc61b7bda8b811fb9ff139fd
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-06 23:41:42 +0530

    Time: 20 ms (79.76%), Space: 11 MB (36.83%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        vector<pair<int, char>> stack = {{0, '#'}};
-        for (char c: s) 
+        //for each char, maintain the count of consecutive chars using a pair
+        vector<pair<int, char>> stack = {{0, '#'}}; //init with dummy data with count 0
+        for(char c: s) 
         {
-            if (stack.back().second != c) 
+            //if prev char is not same is current, add new char to stack with consecutive count 1
+            if(stack.back().second != c) 
             {
                 stack.push_back({1, c});
             }
-            else if (++stack.back().first == k)
-            {
-                stack.pop_back();
+            else 
+            { 
+                stack.back().first++; //increment consecutive count if curr char and prev char are equal
+                if(stack.back().first == k) //if required consecutive counts are met, remove the element from stack as we dont need it
+                    stack.pop_back();
             }
         }
         string res;
-        for (auto &p : stack) 
+        for(auto &p: stack) 
         {
-            res.append(p.first, p.second);
+            res.append(p.first, p.second); //append the char for the consecutive count number of time
         }
         
         return res;
     }
+
 };
