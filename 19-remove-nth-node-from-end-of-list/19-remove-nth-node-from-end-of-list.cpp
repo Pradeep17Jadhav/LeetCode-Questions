e0ddb9dc154e5cb5b54e42be60e63688493e0e84
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-07 10:07:20 +0530

    Time: 3 ms (88.22%), Space: 10.8 MB (34.25%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,6 +11,40 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        return solution1(head, n);
+        // return solution2(head, n);
+    }
+    
+    //TC: O(N)
+    ListNode* solution1(ListNode* &head, int n)
+    {
+        ListNode* i = head;
+        ListNode* j = head;
+        
+        while(n--) //move the first pointer by N steps
+            j = j->next;
+        
+        //if j is NULL, n should be equal to length of linked list, so head should be removed.
+        if(!j)
+            return head->next;
+
+        //move both pointers till next of first pointer is nullptr. 
+        //now the 2nd pointer should be at node before the node to be deleted.
+        while(j && j->next)
+        {
+            i = i->next;
+            j = j->next;
+        }
+        
+        ListNode* temp = i->next;
+        i->next = i->next->next; //delete the next node
+        delete(temp);
+        return head;
+    }
+    
+    //TC: O(2N)
+    ListNode* solution2(ListNode* &head, int &n)
+    {
         ListNode* ptr = head;
         int count = 0;
         while(ptr)
@@ -21,6 +55,8 @@ public:
         count = count - n;
         ptr = head;
         ListNode* temp;
+        
+        //if count == 0, we have to remove last element from the right side of list, i.e. we have to remove the head of list
         if(!count)
         {
             temp = head->next;
