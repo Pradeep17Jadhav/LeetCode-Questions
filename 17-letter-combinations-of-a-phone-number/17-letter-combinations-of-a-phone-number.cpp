1d998b81b2515bdd06e68f92355f67febc354f47
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-09 23:23:43 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (60.00%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    //Iterative approach
+    vector<string> letterCombinations(string digits) {
+        if(digits == "") 
+            return {};
+        vector<string> mappings{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> ans{""};
+
+        for(auto digit : digits) //take all digits one by one. 
+        {
+            vector<string> extendCombination;
+            for(auto& currentCombination : ans) //use existing answer array and append the chars to create new answer
+            {
+                for(auto newChar : mappings[digit - '2']) // append the digits
+                {
+                    extendCombination.push_back(currentCombination + newChar);                            
+                }
+            }
+            ans = move(extendCombination); // same as ans = extendCombination, just avoids copying each value. 
+            // swap(ans, extendCombination); // we can also use swap instead of move
+        }
+        return ans;
+    }
+};
