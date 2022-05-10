09570a42c86385d7847357360558668863bd6737
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-10 21:16:49 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (83.34%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -10,7 +10,7 @@ public:
         return res;
     }
     
-    void solve(int i, int k, int n, vector<int> temp) {
+    void solve(int i, int k, int n, vector<int> &temp) {
         //if all 'k' numbers are used up and also the required sum is met, add to the result
         if(n == 0 & k == 0) {
             res.push_back(temp);
@@ -21,7 +21,8 @@ public:
             return;
 
         //try adding all numbers from current i to 9
-        for(int j = i; j <= 9; j++) {
+        for(int j = i; j <= 9; j++) 
+        {
             temp.push_back(j);
             solve(j+1, k-1, n-j, temp);
             temp.pop_back();
