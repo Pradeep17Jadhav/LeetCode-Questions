3eb44580d0890af6b57fd2c2d3842b47467e17d5
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-11 21:37:31 +0530

    Time: 4 ms (35.46%), Space: 5.9 MB (85.48%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -3,6 +3,30 @@ public:
     string chars = "aeiou";
     int res = 0;
     int countVowelStrings(int n) {
+        return countVowelStrings_1(n); 
+    }
+    
+    
+    int countVowelStrings_1(int n) {
+        //traverse for required chars
+        for (int i = 0; i <= n; i++) {
+            int sum = 0;
+            
+            //Available chars:
+            //for a -> [a,e,i,o,u]
+            //for e -> [e,i,o,u]
+            //for i -> [i,o,u]
+            //for o -> [o,u]
+            //for u -> [u]
+            for (int j = 0; j <= i; j++) {
+                sum += j + 1;
+                res += sum;
+            }
+        }
+        return res;
+    }
+
+    int countVowelStrings_2(int n) {
         char temp = '#';
         find(n, temp);
         return res;
