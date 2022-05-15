bccd9748cf72cf0455054864e7c53583acaf0547
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-15 20:30:14 +0530

    Time: 163 ms (22.74%), Space: 61.8 MB (43.30%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -14,8 +14,31 @@ public:
     int sum = 0;
     int maxDepth = 0;
     int deepestLeavesSum(TreeNode* root) {
-        findSum(root, 1);
-        return sum;
+        return deepestLeavesSumIterative(root);
+        // findSum(root, 1);
+        // return sum;
+    }
+    
+    int deepestLeavesSumIterative(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int res = 0;
+        
+        while(!q.empty()) {
+            res = 0;
+            int n = q.size();
+            
+            //for all nodes in current depth, add the all child nodes in the queue.
+            //when the queue will become empty, we can consider that the current depth is the deepest.
+            for(int i = 0; i < n; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                res += curr->val;
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+        }
+        return res;
     }
 
     void findSum(TreeNode* node, int currDepth)
