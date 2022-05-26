0df512db8edd531d9ec30b0b10a762ecb1df3629
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-26 23:42:56 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (17.32%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -13,16 +13,39 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-        traverse(root, res);
+        
+        // Solution 1 - Recursive
+        // traverse_recursive(root, res); //uncomment to use
+        
+        // Solution 2 - Iterative using Stack
+        stack<TreeNode*> st;
+        while(root || !st.empty()) {
+            
+            //keep pushing all left nodes in the stack
+            while(root) {
+                st.push(root);
+                root = root->left;
+            }
+            
+            //add value if stack top in res and remove it from stack
+            root = st.top();
+            st.pop();
+            res.push_back(root->val);
+
+            //change root to right subtree root to traverse right subtree
+            root = root->right;
+            
+        }
         return res;
     }
     
-    void traverse(TreeNode* root, vector<int> &res) {
+    //traversal function for solution 1
+    void traverse_recursive(TreeNode* root, vector<int> &res) {
         if(!root)
             return;
         
-        traverse(root->left, res);
+        traverse_recursive(root->left, res);
         res.push_back(root->val);
-        traverse(root->right, res);
+        traverse_recursive(root->right, res);
     }
 };
