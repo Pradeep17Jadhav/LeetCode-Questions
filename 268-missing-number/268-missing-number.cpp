384c6556ea8a4422d17987bb0ac0bb5cd24d539b
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-28 17:27:13 +0530

    Time: 23 ms (68.34%), Space: 17.8 MB (93.84%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -5,11 +5,18 @@ public:
         // return method2(nums);
     }
     
-    //since a^b^b = a,
-    //we can use xor to find missing number
+    // since a^b^b = a,
+    // we can use xor to find missing number
+    // property - a^a^b^b^c^c = (a^a)^(b^b)^(c^c) = 0^0^0 = 0
+    // example -
+    // nums = [0,1,3]
+    // indices =[0,1,2,3]
+    // So, we do (0^1^3) ^ (0^1^2^3) = (0^0)^(1^1)^(2)^(3^3) = 0^0^2^0 = 2
     int method1(vector<int>& nums)
     {
-        int res = nums.size();
+        //start with nums.size() because if nums=[0,1,3], in for loop we will only have [0,1,2] indices out of [0,1,2,3]
+        //to compensate the missing last index (which is always nums.size()), we start the res with nums.size()
+        int res = nums.size(); 
         for(int i = 0; i < nums.size(); i++)
         {
             res = res ^ i ^ nums[i];
