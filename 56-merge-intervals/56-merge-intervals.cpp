46586a02d87823c554359f2dd6fa004c105d307c
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-29 18:39:45 +0530

    Time: 90 ms (14.40%), Space: 18.8 MB (94.38%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(intervals.size() <= 1) return intervals;
+        if(intervals.size() <= 1) 
+            return intervals;
+        
+        //sort by 0-index element of all intervals
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> newIntervals;
-        newIntervals.push_back(intervals[0]);
+        
+        vector<vector<int>> res;
+        res.push_back(intervals[0]); //first interval will always be there is result as it has lowest starting range
+
         for(int i = 1; i < intervals.size(); i++)
         {
-            if(newIntervals.back()[1] >= intervals[i][0])
-                newIntervals.back()[1] = max(newIntervals.back()[1], intervals[i][1]);
+            //check if end-range of last interval in result is greater or equal to start-range of current interval
+            //if yes, then update the end-range of last interval
+            //if not, then the previous interval has ended so add current interval to res as a new interval
+            if(res.back()[1] >= intervals[i][0])
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
             else
-                newIntervals.push_back(intervals[i]);
+                res.push_back(intervals[i]);
         }
-        return newIntervals;
+        return res;
     }
 };
