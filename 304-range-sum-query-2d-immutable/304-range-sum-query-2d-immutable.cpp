9430bec7714eaa7cc9b8277ee3bdfbad97fc535f
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-03 22:44:51 +0530

    Time: 629 ms (44.22%), Space: 148 MB (79.01%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -4,28 +4,27 @@ public:
     NumMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        mat = vector<vector<int>>(m+1, vector<int>(n+1)); 
+        mat = matrix; 
         
-        //Set mat[i][j] as a sum of all elements inside the rectangle [0,0,i,j]
-        for(int i = 1; i <= m; i++) 
-        {
-            for (int j = 1; j <= n; j++) 
-            {
-                mat[i][j] = mat[i - 1][j] + mat[i][j - 1] - mat[i - 1][j - 1] + matrix[i - 1][j - 1];
-                cout << mat[i][j] << " ";
+        //find prefix sum of all the columns in each row
+        for(int i = 0; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                mat[i][j] = mat[i][j] + mat[i][j-1];
             }
-            cout << endl;
+        }
+        
+        //find prefix sum of all the rows in each column
+        for(int i = 1; i < m; i++) {
+            for(int j = 0; j < n; j++) { 
+                mat[i][j] = mat[i][j] + mat[i-1][j];
+            }
         }
     }
     int sumRegion(int r1, int c1, int r2, int c2) {
-		// Since our 'mat' starts by 1 so we need to increase r1, c1, r2, c2 by 1
-        r1++; c1++; r2++; c2++; 
-        return mat[r2][c2] - mat[r2][c1 - 1] - mat[r1 - 1][c2] + mat[r1 - 1][c1 - 1];
+        int total = mat[r2][c2]; //bottom right of each rect will have sum of all elements from (0,0) to (r2,c2)
+        int topExtra = r1 == 0 ? 0 : mat[r1-1][c2]; //sum of all elements above the rect is stored in (r1-1, c2)
+        int leftExtra = c1 == 0 ? 0 : mat[r2][c1-1]; //sum of all elements on LHS of rect is stored in (r2, c1-1)
+        int overlapping = c1 != 0 && r1 != 0 ? mat[r1-1][c1-1] : 0; //as we consider both above & left elements, some part is counted twice
+        return total - (topExtra + leftExtra - overlapping);
     }
 };
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
