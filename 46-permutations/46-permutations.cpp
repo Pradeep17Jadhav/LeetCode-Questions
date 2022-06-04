4eee082efd12b9d52a7e4d31462ad1ee96efa1c8
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-04 20:27:58 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (78.38%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        return permute_1(nums);
+        // return permute_2(nums);
+    }
+    
+    vector<vector<int>> permute_1(vector<int>& nums) {
+        vector<vector<int>> res;
+        permutation(res, nums, 0);
+        return res;
+    }
+    
+    void permutation(vector<vector<int>> &res, vector<int> &nums, int i) {
+        if(i == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+        for(int j = i; j < nums.size(); j++) {
+            swap(nums[i], nums[j]); //for each element on RHS, swap it with curr ith element to keep it ith position
+            permutation(res, nums, i+1);
+            swap(nums[j], nums[i]); //backtrack
+        }
+    }
+    
+    vector<vector<int>> permute_2(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> res;
         vector<int> curr = nums;
