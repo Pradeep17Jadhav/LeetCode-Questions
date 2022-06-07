bd2e454271189bc89416cfdf06aa697d74a1dae0
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-07 19:35:22 +0530

    Time: 4 ms (38.69%), Space: 8.4 MB (76.35%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,23 +1,63 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        // return recursive(root);
+        // return iterative(root);
+        return MorrisTraversal(root);
+    }
+        
+
+    //Solution 3 - Morris Traversal (IMP)
+    //TC: almost (but not exactly) O(N)
+    //SC: O(1) - only solution with constant SC
+    vector<int> MorrisTraversal(TreeNode* root) {
         vector<int> res;
+        TreeNode* ptr = root;
         
-        // Solution 1 - Recursive
-        // traverse_recursive(root, res); //uncomment to use
+        while(ptr != NULL) {
+            //Case 1 - ptr does not have left subtree
+            if(ptr->left == NULL) {
+                //since left is null in inorder, current node is added to res and them right subtree is traversed
+                res.push_back(ptr->val); 
+                ptr = ptr->right;
+            }
+            //Case 2 - left subtree exists
+            else {
+                TreeNode* newPtr = ptr->left;
+                
+                //find the rightmost node of left subtree of ptr
+                //it may happen that current node is being visited by the "rightmost node of left subtree"
+                //to know if this is true, we traverse the left subtree again and check if rightmost node
+                //points to the current pointer.
+                //if YES (newPtr->right == ptr), then we remove the thread reference and move to right subtree
+                //if NO (newPtr->right != NULL), it means we havent visited left subtree yet, so move to left subtree
+                while(newPtr->right != NULL && newPtr->right != ptr) {
+                    newPtr = newPtr->right;
+                }
+                
+                //it means we are comming from left subtree left left subtree is already added to res.
+                if(newPtr->right == ptr) {
+                    newPtr->right = NULL; //remove reference thread
+                    res.push_back(ptr->val);
+                    ptr = ptr->right;
+                }
+                //in else, we start traversing left subtree
+                //point the rightmost node's next of left subtree to current node i.e. ptr
+                else {
+                    newPtr->right = ptr;
+                    ptr = ptr->left;
+                }
+            }
+        }
+        return res;
         
-        // Solution 2 - Iterative using Stack
+    }
+    
+    
+        
+    // Solution 2 - Iterative using Stack
+    vector<int> iterative(TreeNode* root) {
+        vector<int> res;
         stack<TreeNode*> st;
         while(root || !st.empty()) {
             
@@ -37,6 +77,15 @@ public:
             
         }
         return res;
+    }
+    
+    
+    // Solution 1 - Recursive
+    vector<int> recursive(TreeNode* root) {
+        vector<int> res;
+        traverse_recursive(root, res);
+        return res;
+        
     }
     
     //traversal function for solution 1
