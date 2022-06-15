afd05b60dbf05f29aa90a3a4587c06cf27d77a38
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-15 23:33:52 +0530

    Time: 15 ms (32.98%), Space: 13.6 MB (63.56%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        // return searchRange_1(nums, target);
+        return searchRange_2(nums, target);
+    }
+
+    vector<int> searchRange_1(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() - 1;
         int mid;
@@ -29,5 +34,44 @@ public:
         }
         
         return {-1,-1};
+    }
+
+
+    vector<int> searchRange_2(vector<int>& nums, int target) {
+        int size = nums.size();
+        vector<int> res(2, -1);
+        if(!size)
+            return res;
+        int start = 0;
+        int end = size-1;
+        
+        // Search for the left end position
+        while(start < end)
+        {
+            int mid = (start + end) /2;
+            if (nums[mid] < target)
+                start = mid + 1;
+            else 
+                end = mid;
+        }
+        
+        if(nums[start] != target) //left end not found 
+            return res;
+        else 
+            res[0] = start;
+
+        // Search for the right end position
+        end = size-1;  // We don not have to set 'left' to 0 the second time.
+        while (start < end)
+        {
+            int mid = (start + end)/2 + 1;	// make mid biased to the right
+            if (nums[mid] > target) 
+                end = mid - 1;  
+            else 
+                start = mid;
+        }
+        res[1] = end;
+        
+        return res; 
     }
 };
