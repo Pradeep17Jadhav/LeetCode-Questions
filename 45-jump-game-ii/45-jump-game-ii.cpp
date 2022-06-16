76406685fb6f40d7fb4d42205b6fffe6081ed154
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-16 22:30:49 +0530

    Time: 40 ms (48.37%), Space: 16.5 MB (80.84%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = size(nums), i = 0, maxReachable = 0, lastJumpedPos = 0, jumps = 0;
-        while(lastJumpedPos < n - 1)
+        int maxReachable = 0; //maximum index we can reach using the jumps available at all explored indices including current i
+        int lastJumpedPos = 0; //the index to which we can jump using the index from which we last jumped
+        int jumps = 0;
+        
+        for(int i = 0; i < nums.size()-1; i++)
         { 
+            //keep on finding the maximum index to which we can jump
+            //once we complete a jump, update the maximum jump index for next jump
             maxReachable = max(maxReachable, i + nums[i]);
             if(i == lastJumpedPos) 
             {
                 lastJumpedPos = maxReachable;
                 jumps++;
-            }            
-            i++;
+            }
         }
         return jumps;
     }
