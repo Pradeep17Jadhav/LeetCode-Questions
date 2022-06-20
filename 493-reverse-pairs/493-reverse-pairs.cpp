546e5b8d090fc2b7b33faac08b4e06f8e54d36ae
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-20 20:53:19 +0530

    Time: 1379 ms (15.71%), Space: 237.4 MB (7.88%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums, 0, nums.size()-1);
+    }
+    
+    int mergeSort(vector<int> &arr, int start, int end) {
+        if(start >= end)
+            return 0;
+
+        int mid = (start + end) / 2;
+        int reversePairCount = mergeSort(arr, start, mid);
+        reversePairCount += mergeSort(arr, mid+1, end);
+        reversePairCount += merge(arr, start, mid, end);
+
+        return reversePairCount;
+    }
+    
+    int merge(vector<int> &arr, int start, int mid, int end) {
+        int reversePairCount = 0;
+        int i, j;
+        i = start;
+        j = mid+1;
+        while(i <= mid) {
+            while(j <= end && arr[i] > 2LL * arr[j])
+                j++;
+
+            reversePairCount += j - (mid+1);
+            i++;
+        }
+
+        i = start;
+        j = mid+1;
+        vector<int> arrNew;
+        
+        //traverse both parts and keep adding smaller element in new array
+        while(i <= mid && j <= end) {
+            if(arr[i] <= arr[j]) {
+                arrNew.push_back(arr[i++]);
+            }
+            else {
+                arrNew.push_back(arr[j++]);
+            }
+        }
+
+        //once one of the part is exhausted, we will have to add remaining elements of other part to the array
+        while(i <= mid) {
+            arrNew.push_back(arr[i++]);
+        }
+
+        while(j <= end) {
+            arrNew.push_back(arr[j++]);
+        }
+
+        //copy back the sorted array to original array
+        for(i = start; i <= end; i++)
+            arr[i] = arrNew[i - start];
+        
+        return reversePairCount;
+    }
+};
