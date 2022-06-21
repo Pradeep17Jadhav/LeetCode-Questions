c6411f02cbbabbef75104c7923f8a2b19907a016
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-21 19:32:50 +0530

    Time: 120 ms (87.35%), Space: 54 MB (14.85%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
+    //Intuition -
+    //There are limited bricks and ladders
+    //But one ladder can be used to cross a huge gap in height
+    //So we should only use ladder for the biggest height differences
+    //Initially We will start using only bricks till they are exhausted
+    //We can use priority queue to keep track of differences of heights where bricks are used
+    //Once bricks are exhausted, we will replace the greatest difference with a single ladder and add bricks used there to remaining bricks.
+    //Continue this till all ladders are exhausted and also the bricks are insufficient
+    
+    //TC: O(N)
+    //SC: O(N) for PQ
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         int res = 0;
         priority_queue<int> pq;
         for(int i = 1; i < heights.size(); i++) {
+            //if we need to jump higher
             if(heights[i] > heights[i-1]) {
-                int diff = heights[i] - heights[i-1];
+                int diff = heights[i] - heights[i-1]; //total bricks needed
+                
+                //if we have required bricks, use them and add the quantity in PQ
                 if(bricks >= diff) {
                     pq.push(heights[i] - heights[i-1]);
                     bricks -= diff;
                 }
+                
+                //if bricks are exhausted but ladders are available
                 else if(ladders) {
+                    //if the heighest total number of bricks used is sufficient for current difference
                     if(pq.size() && pq.top() >= diff) {
                         bricks += pq.top();
                         pq.pop();
@@ -18,6 +35,7 @@ public:
                         i--;
                         continue;
                     }
+                    //if the heighest total number of bricks used cannot be used to replace current requirement
                     ladders--;
                 } 
                 else
