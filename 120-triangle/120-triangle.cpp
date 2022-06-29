350146b5a2899ab14dc4176da1779ef7613ad911
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-29 19:17:55 +0530

    Time: 19 ms (7.18%), Space: 8.7 MB (47.68%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,19 +2,33 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         //Memoization
-        vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size(), -1));
-        return recursion(triangle, dp, 0, 0);
+        // vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size(), -1));
+        // return memoization(triangle, dp, 0, 0);
+        
+        //Tabulation
+        return tabulation(triangle);
     }
-    
-    int recursion(vector<vector<int>> &triangle, vector<vector<int>> &dp, int row, int col) {
+        
+    int tabulation(vector<vector<int>> &triangle) {
+        vector<int> dp(triangle.size()+1);
+        
+        for(int i = triangle.size()-1; i >= 0; i--) {
+            for(int j = 0; j < triangle[i].size(); j++) {
+                dp[j] = min(dp[j], dp[j+1]) + triangle[i][j];
+            }
+        }
+        return dp[0];
+    }
+
+    int memoization(vector<vector<int>> &triangle, vector<vector<int>> &dp, int row, int col) {
         if(row == triangle.size() - 1)
             return triangle[row][col];
 
         if(dp[row][col] != -1)
             return dp[row][col];
 
-        int bottom = recursion(triangle, dp, row+1, col);
-        int diagonal = recursion(triangle, dp, row+1, col+1);
+        int bottom = memoization(triangle, dp, row+1, col);
+        int diagonal = memoization(triangle, dp, row+1, col+1);
 
         return dp[row][col] = min(bottom, diagonal) + triangle[row][col];
     }
