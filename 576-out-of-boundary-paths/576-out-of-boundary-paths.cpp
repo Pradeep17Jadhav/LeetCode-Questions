885f756340d1670916a2c7708334a4a86343b9e3
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-07-16 21:38:50 +0530

    Time: 12 ms (67.75%), Space: 9.2 MB (54.93%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -4,10 +4,10 @@ public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         if(maxMove == 0) return 0;
         vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove+1, -1)));
-        return traverse(dp, m, n, maxMove, startRow, startColumn);
+        return memoization(dp, m, n, maxMove, startRow, startColumn);
     }
-    
-    int traverse(vector<vector<vector<int>>> &dp, int m, int n, int moves, int row, int col) {
+
+    int memoization(vector<vector<vector<int>>> &dp, int m, int n, int moves, int row, int col) {
         if(row < 0 || row >= m || col < 0 || col >= n)
             return 1;
         
@@ -17,10 +17,10 @@ public:
         if(dp[row][col][moves] != -1)
             return dp[row][col][moves];
         int paths = 0;
-        paths = (paths + traverse(dp, m, n, moves-1, row, col-1)) % mod;
-        paths = (paths + traverse(dp, m, n, moves-1, row-1, col)) % mod;
-        paths = (paths + traverse(dp, m, n, moves-1, row, col+1)) % mod;
-        paths = (paths + traverse(dp, m, n, moves-1, row+1, col)) % mod;
+        paths = (paths + memoization(dp, m, n, moves-1, row, col-1)) % mod;
+        paths = (paths + memoization(dp, m, n, moves-1, row-1, col)) % mod;
+        paths = (paths + memoization(dp, m, n, moves-1, row, col+1)) % mod;
+        paths = (paths + memoization(dp, m, n, moves-1, row+1, col)) % mod;
 
         dp[row][col][moves] = paths;
         return paths % mod;
