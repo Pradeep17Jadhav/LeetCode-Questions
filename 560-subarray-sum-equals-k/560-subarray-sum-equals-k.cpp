4c4c42e74c2d26bea5fdfe678a32fb2dbb4bfa20
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-07-18 16:53:23 +0530

    Time: 167 ms (31.34%), Space: 41.5 MB (45.21%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     //reference - https://www.youtube.com/watch?v=HbbYPQc-Oo4
-    //TC: O(N^2)
+    //Use prefix sum and hashmap
+    //Traverse from left to right and keep finding prefix sum
+    //Use hashmap to store the (diff betweek prefix sum - k) and its occurrences 
+    //While traversing, for each element, check the diff between current prefix sum and K.
+    //If this diff is already in hashmap, it means their exists a subarray from last position in hashmap, which equals K
+    //If there are multiple occurrences in hashmap, it means there are same multiple subarrays possible
+    //TC: O(N)
+    //SC: O(N)
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>hash;
         int res = 0;
-        int cumm = 0; //to store cummilative/prefix sum
+        int cumm = 0; //to store cummulative/prefix sum
         int i = 0;
-        hash[0]++; // to count subarrays which starts at index 0;
         while(i < nums.size())
         {
             cumm += nums[i];
+            
+            if(cumm == k) //handle subarray starting from index 0
+                res += 1;
+            
             if(hash.find(cumm - k) != hash.end()) //can work without this but also, but space complexity will increase
-                res += hash[cumm - k];
+                res += hash[cumm - k]; //add total occurences in result
             hash[cumm]++;
             i++;
         }
