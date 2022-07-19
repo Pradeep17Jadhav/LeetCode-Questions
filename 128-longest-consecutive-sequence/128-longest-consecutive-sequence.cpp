87cf7656a01d1e36664cf0eb49fa6c4b3d211e36
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-07-19 19:48:46 +0530

    Time: 659 ms (32.35%), Space: 45.7 MB (73.00%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -7,6 +7,10 @@ public:
         
         for(int i = 0; i < nums.size(); i++)
         {
+            //skip finding the consecutive elements if previous number in consecutive sequence exists in set
+            //example - for [100,2,200,1,4,3], we start from left
+            //when we reach 2, its useless to find 2,3,4 sequence as when we reach 1, we will find bigger sequence 1,2,3,4 
+            //so to reduce TC, we will consider only the starting element of each consecutive sequence
             if(s.find(nums[i]-1) != s.end())
                 continue;
             else
@@ -14,6 +18,7 @@ public:
                 int count = 0;
                 int curr = nums[i];
                 
+                //starting at current element in loop, check how many next consecutive numbers are present in the set
                 while(s.find(curr) != s.end())
                 {
                     count++;
