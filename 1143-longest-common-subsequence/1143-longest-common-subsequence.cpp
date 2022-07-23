c66373b459d7a8898cf0d7b497f81f8758181705
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-07-23 14:39:13 +0530

    Time: 29 ms (63.38%), Space: 13.1 MB (21.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,10 +3,33 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size();
         int n = text2.size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return memoization(text1, text2, dp, m-1, n-1);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return memoization(text1, text2, dp, m-1, n-1);
+        
+        return tabulation(text1, text2, m, n);
     }
     
+    
+    int tabulation(string text1, string text2, int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        
+        for(int i1 = 0; i1 <= m; i1++)
+            dp[i1][0] = 0;
+        
+        for(int i2 = 0; i2 <= n; i2++)
+            dp[0][i2] = 0;
+
+        for(int i1 = 1; i1 <= m; i1++) {
+            for(int i2 = 1; i2 <= n; i2++) {
+                if(text1[i1-1] == text2[i2-1])
+                    dp[i1][i2] = 1 + dp[i1-1][i2-1];
+                else
+                    dp[i1][i2] = max(dp[i1-1][i2], dp[i1][i2-1]);
+            }
+        }
+        
+        return dp[m][n];
+    }
     
     int memoization(string &a, string &b, vector<vector<int>> &dp, int i1, int i2) {
         if(i1 < 0 || i2 < 0)
