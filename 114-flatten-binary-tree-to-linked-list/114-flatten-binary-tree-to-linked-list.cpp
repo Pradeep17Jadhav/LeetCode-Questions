4e5992fa0d32085eae6c7667e074b54cf333120e
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-07-27 23:46:51 +0530

    Time: 12 ms (39.80%), Space: 12.7 MB (38.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     //Steps -
     //It doesnt has left node, do nothing
-    //If it has left node -
+    //If it has left node (Follow inline comments to understand each step) -
     //      - Save right node in temp
     //      - Copy left node to right
     //      - Go to rightmost node of right
     //      - Attach rightmost node to temp (right of current root)
     //      - Make right node of root as new root, and repeat
+
     void flatten(TreeNode* root) {
         if(!root) return;
 
-        TreeNode* temp = root->right;    
-        root->right = root->left;
+        TreeNode* temp = root->right; //Save right node in temp
+        root->right = root->left; //Copy left node to right
         root->left = NULL;
-        TreeNode* ptr = root;
+        TreeNode* ptr = root; //preserve root for traversal
 
+        //Go to rightmost node of right
         while(ptr->right)
             ptr = ptr->right;
 
-        ptr->right = temp;
-        root = root->right;
+        ptr->right = temp; //Attach rightmost node to temp (right of current root)
+        root = root->right; //Make right node of root as new root, and repeat
         flatten(root);
     }
 };
