ceef226703d37718c9f5d6db295b92ed17e9e47a
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-07-31 22:15:10 +0530

    Time: 32 ms (58.19%), Space: 7.4 MB (76.35%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,11 +1,35 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-    //     vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
-    //     return memoization(coins, dp, coins.size()-1, amount);
+//         vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
+//         return memoization(coins, dp, coins.size()-1, amount);
     
-        return tabulation(amount, coins);
+//         return tabulation(amount, coins);
+
+        return tabulation_space_optimized(amount, coins);
     }
+    
+    int tabulation_space_optimized(int amount, vector<int>& coins) {
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
+        
+        for(int j = 0; j <= amount; j++)
+            prev[j] = j % coins[0] == 0;
+        
+        for(int ind = 1; ind < coins.size(); ind++) {
+            for(int target = 0; target <= amount; target++) {
+                int notTake = prev[target];
+                int take = 0;
+                if(coins[ind] <= target)
+                    take = curr[target-coins[ind]];
+                
+                curr[target] = take + notTake;
+            }
+            prev = curr;
+        }
+        
+        return prev[amount];
+    }
+    
     
     int tabulation(int amount, vector<int>& coins) {
         vector<vector<int>> dp(coins.size(), vector<int>(amount+1, 0));
