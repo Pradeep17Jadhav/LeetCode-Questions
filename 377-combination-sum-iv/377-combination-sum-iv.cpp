9d1dc94c776c936950344823dc362f60f97dfd36
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-08-06 23:18:00 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-int combinationSum4(vector<int>& nums, int target) {
-        vector<uint> dp(target + 1); dp[0] = 1;
-        for(int curTarget = 1; curTarget <= target; curTarget++) {
-            for (auto& num : nums) { // check for all elements from nums
-                if (num <= curTarget)  // an element will only be taken if it is less than target to be achieved
-                    // the optimal answer(no. of combination) of remaining value is added to dp[curTarget]
-                    dp[curTarget] += dp[curTarget - num];   
-            }     
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, -1);
+        return memoization(nums, dp, target);
+    }
+    
+    int memoization(vector<int> &nums, vector<int> &dp, int target) {
+        if(target == 0)
+            return 1;
+
+        if(dp[target] != -1)
+            return dp[target];
+
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] <= target)
+                count += memoization(nums, dp, target-nums[i]);
+            
         }
-        return dp[target];
+        
+        return dp[target] = count;
+            
     }
 };
