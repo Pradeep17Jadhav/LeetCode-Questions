30508020f36f85044c92ac8f3c72c91552fc650c
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-08-13 12:54:17 +0530

    Time: 38 ms (75.65%), Space: 23.4 MB (12.34%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    //We will take advantage of the tree being a BST
+    //if values of both the P and Q is less the root, we will move to left subtree and ignore right subtree
+    //If values of both the P and Q is grater than the root, we will move to right subtree
+    //When P and Q and either lesser or greater than root, we can consider the current root as lowest common ancestor
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root->val > p->val && root->val > q->val)
             return lowestCommonAncestor(root->left, p, q);
