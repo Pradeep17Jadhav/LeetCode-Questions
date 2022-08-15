f70d12f3219c309235c20935041080334f607ade
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-08-15 22:33:38 +0530

    Time: 38 ms (13.25%), Space: 9 MB (6.41%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        char last = 'Z';
+        unordered_map<char, int> mp {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+            
         int res = 0;
-        for(char &c : s) {
-            if(c == 'M') {
-                res += 1000;
-                if(last == 'C')
-                    res -= 200;
-            }
-            else if(c == 'D') {
-                res += 500;
-                if(last == 'C')
-                    res -= 200;
-            }
-            else if(c == 'C') {
-                res += 100;
-                if(last == 'X')
-                    res -= 20;
-            }
-            else if(c == 'L') {
-                res += 50;
-                if(last == 'X')
-                    res -= 20;
-            }
-            else if(c == 'X') {
-                res += 10;
-                if(last == 'I')
-                    res -= 2;
-            }
-            else if(c == 'V') {
-                res += 5;
-                if(last == 'I')
-                    res -= 2;
-            }
+        for(int i = 0; i < s.size(); i++) {
+            if(mp[s[i]] < mp[s[i+1]])
+                res -= mp[s[i]];
             else
-                res += 1;
-
-            cout << res << " ";
-            last = c;
+                res += mp[s[i]];
         }  
         
         return res;
